71c13d0bde3eccfa7a6fcdf844c69d92a51c29fe
Raajn Patel <rndpatel@gmail.com>
2019-08-19 11:17:23 -0700

    Semi functional app working. Not sure what the end goal is exactly..

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,41 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import MovieCard from "./MovieCard";
+import MovieCard from './MovieCard';
 
-const Movie = () => {
+const Movie = (props) => {
     const [movie, setMovie] = useState({});
 
-    useEffect((props) => {
+    useEffect(() => {
         const id = props.match.params.id;
-        // change ^^^ that line and grab the id from the URL
-        // You will NEED to add a dependency array to this effect hook
 
         axios
             .get(`http://localhost:5000/api/movies/${id}`)
             .then(response => {
+                console.log(response);
                 setMovie(response.data);
             })
             .catch(error => {
                 console.error(error);
             });
 
-    }, []);
+    });
 
     // Uncomment this only when you have moved on to the stretch goals
-    const saveMovie = (props) => {
-      const addToSavedList = props.addToSavedList;
-      addToSavedList(movie)
-    };
+    const saveMovie = () => {
+        const addToSavedList = props.addToSavedList;
+        addToSavedList(movie)
+    }
 
     if (!movie) {
         return <div>Loading movie information...</div>;
     }
+
     return (
         <div className="save-wrapper">
             <MovieCard movie={movie} />
             <div className="save-button" onClick={saveMovie}>Save</div>
         </div>
-    )
-};
+    );
+}
 
 export default Movie;
