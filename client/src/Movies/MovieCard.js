abbd3311b78a92db75d6a2c17c47ba5f79ce5fed
Raajn Patel <rndpatel@gmail.com>
2019-08-19 10:22:08 -0700

    Working on getting cards to be clickable.

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import {  Link } from "react-router-dom";
-import Movie from "./Movie";
 
 const MovieCard = props => {
-  return;
+  const { title, director, metascore, stars } = props.movie;
+  return (
+      <div className="save-wrapper">
+       <Link to = {`movies/${props.movie.id}`}><div className="movie-card">
+          <h2>{title}</h2>
+          <div className="movie-director">
+            Director: <em>{director}</em>
+          </div>
+          <div className="movie-metascore">
+            Metascore: <strong>{metascore}</strong>
+          </div>
+          <h3>Actors</h3>
+
+          {stars.map(star => (
+              <div key={star} className="movie-star">
+                {star}
+              </div>
+          ))}
+        </div>
+           </Link>
+        <div className="save-button">Save</div>
+      </div>
+  );
 };
 
 export default MovieCard;
